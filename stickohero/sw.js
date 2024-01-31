80f9181ccd1ea20415cc4b57228f9a017e93d78f
Muhaimin A. Salay Kanton <126319369+heli-toon@users.noreply.github.com>
2024-01-31 22:16:11 +0000

    Added stickohero to collection

---
+++ stickohero/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./style.css",
+        "./main.js",
+        "./favicon.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
